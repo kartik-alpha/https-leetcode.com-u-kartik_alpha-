66a893789424f44f9c14e75a7df0d3646c15a33a
kartik <146855365+kartik-alpha@users.noreply.github.com>
2026-07-08 17:23:54 +0530

    Time: 88 ms (6.67%), Space: 151.6 MB (6.67%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -4,100 +4,64 @@ class Solution {
     static final long MOD = 1_000_000_007L;
 
     public int[] sumAndMultiply(String s, int[][] queries) {
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> dig = new ArrayList<>();
 
-        int n = s.length();
-
-        ArrayList<Integer> poss = new ArrayList<>();
-        ArrayList<Integer> digit = new ArrayList<>();
-
-        // Store non-zero digits and their positions
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.length(); i++) {
             int d = s.charAt(i) - '0';
             if (d != 0) {
-                poss.add(i);
-                digit.add(d);
+                pos.add(i);
+                dig.add(d);
             }
         }
 
-        int m = digit.size();
+        int m = dig.size();
+        long[] pre = new long[m + 1];
+        long[] sum = new long[m + 1];
+        long[] pow = new long[m + 1];
+        pow[0] = 1;
 
-        long[] prefvalue = new long[m + 1];
-        long[] prefsum = new long[m + 1];
-        long[] pow10 = new long[m + 1];
-
-        pow10[0] = 1;
-
-        // Precompute powers of 10
         for (int i = 1; i <= m; i++) {
-            pow10[i] = (pow10[i - 1] * 10) % MOD;
-        }
-
-        // Prefix value and prefix digit sum
-        for (int i = 1; i <= m; i++) {
-            prefvalue[i] = (prefvalue[i - 1] * 10 + digit.get(i - 1)) % MOD;
-            prefsum[i] = prefsum[i - 1] + digit.get(i - 1);
+            pow[i] = pow[i - 1] * 10 % MOD;
+            pre[i] = (pre[i - 1] * 10 + dig.get(i - 1)) % MOD;
+            sum[i] = sum[i - 1] + dig.get(i - 1);
         }
 
         int[] ans = new int[queries.length];
 
         for (int i = 0; i < queries.length; i++) {
+            int l = lower(pos, queries[i][0]);
+            int r = upper(pos, queries[i][1]) - 1;
 
-            int l = queries[i][0];
-            int r = queries[i][1];
+            if (l > r) continue;
 
-            int L = lowerBound(poss, l);
-            int R = upperBound(poss, r) - 1;
+            int len = r - l + 1;
+            long x = (pre[r + 1] - pre[l] * pow[len] % MOD + MOD) % MOD;
+            long sm = sum[r + 1] - sum[l];
 
-            if (L > R) {
-                ans[i] = 0;
-                continue;
-            }
-
-            int len = R - L + 1;
-
-            long sum = prefsum[R + 1] - prefsum[L];
-
-            long x = (prefvalue[R + 1]
-                    - (prefvalue[L] * pow10[len]) % MOD
-                    + MOD) % MOD;
-
-            ans[i] = (int) ((x * (sum % MOD)) % MOD);
+            ans[i] = (int) (x * (sm % MOD) % MOD);
         }
 
         return ans;
     }
 
-    // First index >= target
-    private int lowerBound(ArrayList<Integer> list, int target) {
-        int s = 0;
-        int e = list.size();
-
-        while (s < e) {
-            int mid = s + (e - s) / 2;
-
-            if (list.get(mid) >= target)
-                e = mid;
-            else
-                s = mid + 1;
+    private int lower(ArrayList<Integer> a, int x) {
+        int l = 0, r = a.size();
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (a.get(m) >= x) r = m;
+            else l = m + 1;
         }
-
-        return s;
+        return l;
     }
 
-    // First index > target
-    private int upperBound(ArrayList<Integer> list, int target) {
-        int s = 0;
-        int e = list.size();
-
-        while (s < e) {
-            int mid = s + (e - s) / 2;
-
-            if (list.get(mid) > target)
-                e = mid;
-            else
-                s = mid + 1;
+    private int upper(ArrayList<Integer> a, int x) {
+        int l = 0, r = a.size();
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (a.get(m) > x) r = m;
+            else l = m + 1;
         }
-
-        return s;
+        return l;
     }
 }
