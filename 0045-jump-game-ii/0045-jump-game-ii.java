ed67551c4e30ad0d11897acfa31a35c6c130aa02
kartik <146855365+kartik-alpha@users.noreply.github.com>
2026-07-22 11:32:10 +0530

    Time: 1 ms (99.72%), Space: 47.5 MB (21.51%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int jump(int[] nums) {
+
+        if(nums.length <= 1 ) {
+            return 0;
+        }
+
+        int jump  = 0;
+        int current = 0;
+        int farthest = 0;
+
+         for(int i = 0; i < nums.length - 1 ; i++) {
+
+             farthest = Math.max(farthest , i + nums[i]);
+
+
+            if(i == current) {
+                jump++;
+                current = farthest;
+            
+
+            if(farthest >= nums.length - 1 ) {
+                break;
+            }
+         }
+         }
+         return jump;
+        
+    }
+}
