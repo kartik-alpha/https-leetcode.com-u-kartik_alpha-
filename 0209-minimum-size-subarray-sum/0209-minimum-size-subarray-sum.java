883105b096826c94add20bb136ef856c53702ce2
kartik <146855365+kartik-alpha@users.noreply.github.com>
2026-08-13 17:05:12 +0530

    Time: 2 ms (20.75%), Space: 69.4 MB (10.69%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,7 +1,11 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
 
-
+   if(target == 0)
+         return 0;
+        if(nums.length == 0  || nums == null) {
+            return 0;
+        }
         int left = 0;
         int sum = 0;
         int answer = nums.length + 1;
@@ -19,8 +23,7 @@ class Solution {
             }
         }
 
-        if(answer == nums.length + 1)  return 0;
-
+      if(answer == nums.length + 1)  return 0;
 
         return answer;
     }
