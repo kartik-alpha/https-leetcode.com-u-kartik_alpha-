62bf401b58f30f7e417a1ed7f7ae0eb99546ec85
kartik <146855365+kartik-alpha@users.noreply.github.com>
2026-09-01 08:30:06 +0530

    Time: 7 ms (99.97%), Space: 48 MB (5.49%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,11 +1,11 @@
 class NumArray {
 
-    private int[] prefixSum;
+    public int[] prefixSum;
 
     public  NumArray(int[] nums) {
 
         prefixSum   = new int[nums.length + 1 ];
-        
+
         for(int i = 0; i < nums.length;i++) {
             prefixSum[i + 1]  = prefixSum[i] + nums[i];
         }
